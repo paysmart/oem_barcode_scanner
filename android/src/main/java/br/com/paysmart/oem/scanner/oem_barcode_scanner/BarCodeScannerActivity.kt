835fe3c91460265ebafb2d89b36789e1eb4ec96f
Bruno Giacobo Pinto <bruno.pinto@paysmart.com.br>
2020-09-17 06:28:16 -0300

    feat(plugin): add initial barcode scanner impl
    Add initial barcode scanner impl

--- android/src/main/java/br/com/paysmart/oem/scanner/oem_barcode_scanner/BarCodeScannerActivity.kt
+++ android/src/main/java/br/com/paysmart/oem/scanner/oem_barcode_scanner/BarCodeScannerActivity.kt
@@ -1,20 +1,50 @@
 package br.com.paysmart.oem.scanner.oem_barcode_scanner
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 import kotlinx.android.synthetic.main.activity_bar_code_scanner.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.Manifest.permission.CAMERA
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import android.os.Bundle
+import android.util.Log
 
 
 class BarCodeScannerActivity : AppCompatActivity() {
+
+    private fun askCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(CAMERA), 0x0)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_code_scanner)
 
-        intent?.getStringExtra("color")?.let { color ->
-            colorHeadline?.text = color
+        askCameraPermission()
+
+        barcodeSurface.setStatusText("")
+        barcodeSurface.decodeContinuous { barcode ->
+            Log.d("________", "Decoder read = ${barcode.text}")
         }
 
+
+        /*
+        intent?.getStringExtra("color")?.let { color ->
+            colorHeadline?.text = color
+        }*/
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        barcodeSurface.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        barcodeSurface.pause()
     }
 }

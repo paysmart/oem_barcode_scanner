5aab4dd044282385e962540d67628ddfdac7c2d8
NatashaZettler <natasha_ksf@outlook.com>
2020-09-18 03:35:59 -0300

    feat(Camera&Layout):camera orientation and layout

--- example/ios/Runner/BarCodeScannerViewController.swift
+++ example/ios/Runner/BarCodeScannerViewController.swift
@@ -5,138 +5,148 @@
 //  Created by Natasha Zettler on 17/09/20.
 //
 
-    import UIKit
-    import AVFoundation
-     
-    class BarCodeScannerViewController: UIViewController {
+import UIKit
+import AVFoundation
+
+class BarCodeScannerViewController: UIViewController {
+    
+    var avCaptureSession: AVCaptureSession!
+    var avPreviewLayer: AVCaptureVideoPreviewLayer!
+    var result : FlutterResult!
+    var color : String!
+    @IBOutlet weak var barCode: UIButton!
+    @IBOutlet weak var back: UIButton!
+    @IBOutlet weak var viewCamera: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.barCode.backgroundColor = hexStringToUIColor(hex: color)
+        self.barCode.layer.cornerRadius = 0.5
         
-        var avCaptureSession: AVCaptureSession!
-        var avPreviewLayer: AVCaptureVideoPreviewLayer!
-        var result : FlutterResult!
-        var color : String!
-        @IBOutlet weak var barCode: UIButton!
-        @IBOutlet weak var back: UIButton!
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            self.barCode.backgroundColor = hexStringToUIColor(hex: color)
-            avCaptureSession = AVCaptureSession()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
-                    self.failed()
-                    return
-                }
-                let avVideoInput: AVCaptureDeviceInput
-                
-                do {
-                    avVideoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-                } catch {
-                    self.failed()
-                    return
-                }
-                
-                if (self.avCaptureSession.canAddInput(avVideoInput)) {
-                    self.avCaptureSession.addInput(avVideoInput)
-                } else {
-                    self.failed()
-                    return
-                }
-                
-                let metadataOutput = AVCaptureMetadataOutput()
-                
-                if (self.avCaptureSession.canAddOutput(metadataOutput)) {
-                    self.avCaptureSession.addOutput(metadataOutput)
-                    
-                    metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-                    metadataOutput.metadataObjectTypes = [.interleaved2of5]
-                } else {
-                    self.failed()
-                    return
-                }
-                
-                self.avPreviewLayer = AVCaptureVideoPreviewLayer(session: self.avCaptureSession)
-                self.avPreviewLayer.frame = self.view.layer.bounds
-                self.avPreviewLayer.videoGravity = .resizeAspectFill
-                self.view.layer.addSublayer(self.avPreviewLayer)
-                self.avCaptureSession.startRunning()
+        avCaptureSession = AVCaptureSession()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+                self.failed()
+                return
             }
-        }
-        
-        
-        func failed() {
-            let ac = UIAlertController(title: "Scanner not supported", message: "Please use a device with a camera. Because this device does not support scanning a code", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-            avCaptureSession = nil
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
+            let avVideoInput: AVCaptureDeviceInput
             
-            if (avCaptureSession?.isRunning == false) {
-                avCaptureSession.startRunning()
+            do {
+                avVideoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+            } catch {
+                self.failed()
+                return
             }
-        }
-        
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
             
-            if (avCaptureSession?.isRunning == true) {
-                avCaptureSession.stopRunning()
-            }
-        }
-        
-        override var prefersStatusBarHidden: Bool {
-            return true
-        }
-        
-        override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-            return .portrait
-        }
-        
-        func hexStringToUIColor (hex:String) -> UIColor {
-            var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
-            if (cString.hasPrefix("#")) {
-                cString.remove(at: cString.startIndex)
-            }
-
-            if ((cString.count) != 6) {
-                return UIColor.gray
-            }
-
-            var rgbValue:UInt64 = 0
-            Scanner(string: cString).scanHexInt64(&rgbValue)
-
-            return UIColor(
-                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                alpha: CGFloat(1.0)
-            )
-        }
-        
-        @IBAction func backToScreen(_ sender: Any) {
-            if let nav = self.navigationController {
-                nav.popViewController(animated: true)
+            if (self.avCaptureSession.canAddInput(avVideoInput)) {
+                self.avCaptureSession.addInput(avVideoInput)
             } else {
-                self.dismiss(animated: true, completion: nil)
+                self.failed()
+                return
             }
+            
+            let metadataOutput = AVCaptureMetadataOutput()
+            
+            if (self.avCaptureSession.canAddOutput(metadataOutput)) {
+                self.avCaptureSession.addOutput(metadataOutput)
+                
+                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                metadataOutput.metadataObjectTypes = [.interleaved2of5]
+            } else {
+                self.failed()
+                return
+            }
+            
+            self.avPreviewLayer = AVCaptureVideoPreviewLayer(session: self.avCaptureSession)
+            
+            self.avPreviewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight
+            self.avPreviewLayer.frame = self.view.layer.bounds
+            self.avPreviewLayer.videoGravity = .resizeAspectFill
+            self.viewCamera.layer.addSublayer(self.avPreviewLayer)
+            self.avCaptureSession.startRunning()
         }
-        
-        
     }
-    extension BarCodeScannerViewController : AVCaptureMetadataOutputObjectsDelegate {
-        func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanner not supported", message: "Please use a device with a camera. Because this device does not support scanning a code", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        avCaptureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (avCaptureSession?.isRunning == false) {
+            avCaptureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (avCaptureSession?.isRunning == true) {
             avCaptureSession.stopRunning()
-            
-            if let metadataObject = metadataObjects.first {
-                guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-                guard let stringValue = readableObject.stringValue else { return }
-                AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-                result(stringValue)
-            }
-            
-            dismiss(animated: true)
         }
     }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    @IBAction func backToScreen(_ sender: Any) {
+        if let nav = self.navigationController {
+            nav.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+}
+extension BarCodeScannerViewController : AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        avCaptureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            returnValue(value: stringValue)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func returnValue(value: String) {
+        result(value)
+        if let nav = self.navigationController {
+            nav.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+}

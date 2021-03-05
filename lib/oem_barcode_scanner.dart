b7d5f75869d995db0d6011f82a23e40455bb4b06
GManzke <gabrielmzke@gmail.com>
2021-03-05 13:38:50 -0300

    feat(plugin): Release v1.9.0

--- lib/oem_barcode_scanner.dart
+++ lib/oem_barcode_scanner.dart
@@ -9,13 +9,10 @@ class OEMBarcodeScanner {
 
   static Future<String> scanBarCode(String color,
       {String text =
-          'Posicione o código de barras sob\n a linha e aguarde a leitura'}) async {
-    if (Platform.isIOS) {
-      await _channel.invokeMethod('scan', {'color': color});
-    }
-    return await _channel
-        .invokeMethod('scanBarCode', {'color': color, 'text': text});
-  }
+      'Posicione o código de barras sob\n a linha e aguarde a leitura'}) async =>
+      await _channel
+          .invokeMethod('scanBarCode', {'color': color, 'text': text});
+
 
   static Future<String> scanQRCode(String color,
       {String text = 'Posicione o QR Code ELO na\n marcação e aguarde a leitura'}) async =>

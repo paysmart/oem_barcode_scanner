5599363af10753ff0001dad1b031fa52673fd57e
brunobrt <brunobrt@hotmail.com>
2021-05-10 17:49:44 -0300

    feat(plugin): release v2.0.0

--- lib/oem_barcode_scanner.dart
+++ lib/oem_barcode_scanner.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io' show Platform;
 
 import 'package:flutter/services.dart';
 
@@ -8,14 +7,13 @@ class OEMBarcodeScanner {
   static const eventChannel = const EventChannel('oem_barcode_scanner/events');
 
   static Future<String> scanBarCode(String color,
-      {String text =
-      'Posicione o código de barras sob\n a linha e aguarde a leitura'}) async =>
+          {String text =
+              'Posicione o código de barras sob\n a linha e aguarde a leitura'}) async =>
       await _channel
           .invokeMethod('scanBarCode', {'color': color, 'text': text});
 
-
   static Future<String> scanQRCode(String color,
-      {String text = 'Posicione o QR Code ELO na\n marcação e aguarde a leitura'}) async =>
-      await _channel.invokeMethod(
-          'scanQRCode', {'color': color, 'text': text});
+          {String text =
+              'Posicione o QR Code ELO na\n marcação e aguarde a leitura'}) async =>
+      await _channel.invokeMethod('scanQRCode', {'color': color, 'text': text});
 }

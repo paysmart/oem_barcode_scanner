854de9361bd238184c0965dd61081c50aaa07ab5
ViniciusFVianna23 <vinicius.vianna@paysmart.com.br>
2023-02-20 09:46:33 -0300

    fix

--- test/oem_barcode_scanner_test.dart
+++ test/oem_barcode_scanner_test.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:oem_barcode_scanner/oem_barcode_scanner.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('oem_barcode_scanner');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
